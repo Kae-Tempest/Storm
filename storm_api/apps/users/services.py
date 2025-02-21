2d7587ae51156d4ce014706603f4c5e60755149c
Kae <73745621+Kae-Tempest@users.noreply.github.com>
2025-02-21 19:59:02 +0100

    begin user service

--- storm_api/apps/users/services.py
+++ storm_api/apps/users/services.py
@@ -6,3 +6,20 @@ class UsersServices:
     @staticmethod
     def get_users():
         return CustomUser.objects.all()
+
+    @staticmethod
+    def get_user_by_id(user_id):
+        return CustomUser.objects.get(id=user_id)
+
+    @staticmethod
+    def get_user_by_email(email):
+        return CustomUser.objects.get(email=email)
+
+    @staticmethod
+    def get_user_by_tag(tag):
+        return CustomUser.objects.get(tag=tag)
+
+    @staticmethod
+    def update_user(user_id):
+        user = CustomUser.objects.get(id=user_id)
+    
