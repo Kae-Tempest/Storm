a97be936110059049df66b2ecef50aacfd665105
Kae-Tempest <katsuhiko.tempest@gmail.com>
2025-02-24 23:19:48 +0100

    Change display ul section and add icons

--- storm-frontend/src/lib/components/navbar.svelte
+++ storm-frontend/src/lib/components/navbar.svelte
@@ -1,32 +1,49 @@
 <script>
     import logo from "$lib/assets/logo.png";
     import {AuthStore} from "$lib/stores/login/login.js";
+    import {House, MessagesSquare, Plus, Search} from "lucide-svelte";
 
     let showMenu = false;
 </script>
 
-<nav class="h-full max-w-2xs border-r border-r-blue-900 flex flex-col justify-between sticky">
+<nav class="h-full max-w-3xs border-r border-r-blue-900 flex flex-col sticky">
     <section>
         <a href="/" class="flex w-full items-center">
             <img src="{logo}" alt="project logo" class="max-w-[123px]"/>
-            <h1 class="text-4xl px-4 font-black font-[Arial]">Storm</h1>
+            <h1 class="text-4xl font-black">Storm</h1>
         </a>
     </section>
-    <section class="flex flex-col w-full items-center font-black font-[Arial]">
-        <ul class="h-full flex flex-col text-center gap-10 text-2xl">
-            <li><a href="/home">HOME</a></li>
-            <li><a href="/chat">CHAT</a></li>
-            <li>
-                <button>SEARCH</button>
+    <section class="flex flex-col w-full items-center mb-auto ">
+        <ul class="h-full flex flex-col text-xl w-2/3">
+            <li class="hover:bg-stone-100/20 w-full p-2 rounded cursor-pointer">
+                <a href="/" class="flex gap-2">
+                    <House/>
+                    Home
+                </a>
             </li>
-            <li>
-                <button>POST</button>
+            <li class="hover:bg-stone-100/20 w-full p-2 rounded cursor-pointer">
+                <a href="/chat" class="flex gap-2">
+                    <MessagesSquare/>
+                    Chat
+                </a>
+            </li>
+            <li class="hover:bg-stone-100/20 w-full p-2 rounded cursor-pointer">
+                <button class="cursor-pointer flex gap-2">
+                    <Search/>
+                    Search
+                </button>
+            </li>
+            <li class="hover:bg-stone-100/20 w-full p-2 rounded cursor-pointer">
+                <button class="cursor-pointer flex gap-2">
+                    <Plus/>
+                    Post
+                </button>
             </li>
         </ul>
     </section>
-    <section class="flex w-full justify-center">
+    <section class="flex w-full justify-center relative">
         {#if showMenu}
-            <div class="absolute bottom-30 left-13 rounded p-5 text-xl w-2/3 text-center
+            <div class="absolute bottom-30 left-11 rounded p-5 text-xl w-2/3 text-center
              bg-slate-800 border border-blue-900 ">
                 <ul class="flex flex-col gap-2">
                     <li>
